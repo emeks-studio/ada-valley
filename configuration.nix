ad01936406633001baaf7e9e4c67b1112bcf2785
smonsech <santiago@emeks.com.ar>
2025-07-31 14:26:08 -0300

    moved cardano-auditor runnable to alice packages

--- configuration.nix
+++ configuration.nix
@@ -122,6 +122,7 @@
       tree
       cardano-node
       cardano-cli
+      cardano-auditor
     ];
   };
 
@@ -133,10 +134,6 @@
   #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #   wget
   # ];
-
-  environment.systemPackages = with pkgs; [
-    cardano-auditor
-  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
