decdfe2b73aa8cf77ca20d52893505619a6429cc
mk-emeks <mirko.alic@plowtech.net>
2025-08-30 21:08:26 -0300

    chore: Upgrade nixos and cardano-node

--- flake.nix
+++ flake.nix
@@ -17,15 +17,14 @@
 
   description = "NixOS VM configuration";
 
+  # UPGRADE INSTRUCTIONS:
+  # Ref. https://github.com/disassembler/network
+  # ^ I Usually search in the flake.nix of this repo in order to know which versions to use.
+  #   After changing the versions in our flake.nix run `nix flake update` to update the lock file.
   inputs = {
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     sops-nix.url = "github:Mic92/sops-nix";
     impermanence.url = "github:nix-community/impermanence";
-    hackageNix = {
-      url = "github:input-output-hk/hackage.nix?ref=for-stackage";
-      flake = false;
-    };
 
     varsFilePath = {
       # Default to vars-template.nix please make your own vars.nix and override the input!
@@ -33,30 +32,8 @@
       flake = false;
     };
 
-    haskellNix = {
-      # GHC 8.10.7 cross compilation for windows is broken in newer versions of haskell.nix.
-      # Unpin this once we no longer need GHC 8.10.7.
-      url = "github:input-output-hk/haskell.nix/cb139fa956158397aa398186bb32dd26f7318784";
-      inputs.hackage.follows = "hackageNix";
-      # url = "github:input-output-hk/haskell.nix/14f740c7c8f535581c30b1697018e389680e24cb";
-      # ^ error: The option `flake' does not exist. Definition values:
-      #  - In `<unknown-file>':
-      #      {
-      #        variants = {
-      #          ghc96 = {
-      #            compiler-nix-name = "ghc96";
-      #          };
-    };
-
-    #iohkNix = {
-    #  url = "github:input-output-hk/iohk-nix";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
-
-    # Ref. https://github.com/disassembler/network
     cardano-node = {
-      url = "github:intersectmbo/cardano-node/10.1.4";
-      inputs.haskellNix.follows = "haskellNix";
+      url = "github:intersectmbo/cardano-node/10.5.1";
     };
 
     ssh-keys = {
@@ -65,7 +42,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, sops-nix, impermanence, hackageNix, haskellNix, /* iohkNix,*/ cardano-node, varsFilePath, ssh-keys }:
+  outputs = { self, nixpkgs, sops-nix, impermanence, cardano-node, varsFilePath, ssh-keys }:
     let 
       vars = builtins.import varsFilePath;
       system = "x86_64-linux";
@@ -78,13 +55,6 @@
         specialArgs = { inherit vars; };
         modules = [ 
             {  nixpkgs.overlays = [
-                  # Crypto needs to come before haskell.nix. FIXME: _THIS_IS_BAD_
-                  # iohkNix.overlays.crypto
-                  # haskellNix.overlay
-                  # iohkNix.overlays.haskell-nix-extra
-                  # iohkNix.overlays.haskell-nix-crypto
-                  # iohkNix.overlays.cardano-lib
-                  # iohkNix.overlays.utils
                   (prev: final: {
                     cardano-cli = cardano-node.packages.${final.system}.cardano-cli;
                     cardano-node = cardano-node.packages.${final.system}.cardano-node;
